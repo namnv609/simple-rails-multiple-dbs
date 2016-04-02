641888e22b875d8f94c2005ec979490367be72b4
NamNV609 <namnv609@gmail.com>
2016-04-02 16:47:14 +0700

    Add table name prefix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   establish_connection DB2_CONF
+
+  def self.table_name_prefix
+    "#{self.connection.current_database}."
+  end
+
+  # self.table_name = "#{self.connection.current_database}.posts"
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   establish_connection DB1_CONF
+
+  def self.table_name_prefix
+    "#{self.connection.current_database}."
+  end
+
+  # self.table_name = "#{self.connection.current_database}.users"
   has_many :posts
 end
